3973da64a27ece405744574211a1234beac56383
Lawrence Zhang <lawrence.zhang@icloud.com>
2020-09-10 18:26:39 +0800

    add exception handling

--- src/handlers/position.js
+++ src/handlers/position.js
@@ -1,5 +1,17 @@
 class Position {
     constructor(latitude, longitude, address) {
+      if (latitude === '' || isNaN(latitude) || latitude === null) {
+        throw new Error('validation error, latitude can not be empty string or none number type')
+      }
+
+      if (longitude === '' || isNaN(longitude) || longitude === null) {
+        throw new Error('validation error, longitude can not be empty string or none number type')
+      }
+
+      if (address === '' || address === null) {
+        throw new Error('validation error, address can not be empty string or null');
+      }
+
       this.latitude = Number(latitude);
       this.longitude = Number(longitude);
       this.address = address;
--- src/handlers/s3-event.js
+++ src/handlers/s3-event.js
@@ -15,12 +15,16 @@ exports.handler = async (event, context, callback) => {
   const typeMatch = srcKey.match(/\.([^.]*)$/);
   
   if (!typeMatch) {
-      console.log("Could not determine the csv type.");
+      console.error("Could not determine the csv type.");
+      const errorSNS = new SNSMessage(srcBucket, srcKey, "", "Could not determine the csv type.");
+      SNSMessage.publishMessage(errorSNS);
       return;
   }
   const csvType = typeMatch[1].toLowerCase();
   if (csvType != "csv") {
-      console.log(`Unsupported csv type: ${csvType}`);
+      console.error(`Unsupported csv type: ${csvType}`);
+      const errorSNS = new SNSMessage(srcBucket, srcKey, "", "Unsupported csv type");
+      SNSMessage.publishMessage(errorSNS);
       return;
   }  
   const params = {
@@ -28,16 +32,47 @@ exports.handler = async (event, context, callback) => {
     Key: srcKey
   };
   
-  //const ccc = new SNSMessage(1, 1, 1, 1);
- // SNSMessage.publishMessage(ccc);
-  
   const stream = s3.getObject(params).createReadStream();
-  csv.parseStream(stream, { headers: true }).on('error', error => console.error(error))
-    .on('data', (row) => {
+  
+  csv.parseStream(stream.on('error', s3error => {
+    const errorSNS = new SNSMessage(srcBucket, srcKey, "", s3error);
+    SNSMessage.publishMessage(errorSNS);
+    console.error(s3error);
+  }), { headers: true })
+  .on('error', error => {
+    var rawData = '';
+    if (typeof error.rawData !== 'undefined') {
+      rawData = error.rawData;
+    }
+    const errorSNS = new SNSMessage(srcBucket, srcKey, rawData, error.message);
+    SNSMessage.publishMessage(errorSNS);
+    console.error(error);   
+    return;
+  })
+  .on('data', (row) => {
+    try {
+      if (typeof row.latitude === 'undefined' || typeof row.longitude === 'undefined' || typeof row.address === 'undefined') {
+        const formatError = new Error("heads are not matched by standard");
+        formatError.rawData = row;
+        formatError.stopFlag = true;
+        throw formatError;
+      }
       const position = new Position(row.latitude, row.longitude, row.address);
       Position.saveToDynamoDb(position);  
-    })
-    .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
-  
+    }
+    catch(e) {
+      if (typeof e.rawData === 'undefined') {
+        e.rawData = row;
+      }
+      if (e.stopFlag) {
+        throw e;
+      } 
+      else {
+        const errorSNS = new SNSMessage(srcBucket, srcKey, row, e.message);
+        SNSMessage.publishMessage(errorSNS);
+        console.error(e);
+      }
+    }
+  });
 };
 
